29d1fa816485713934140837366c71e81220a6db
unknown <qsnahuel@gmail.com>
2025-07-17 01:58:33 -0300

    creacion repositorio

---
+++ src/main/java/com/unlu/alimtrack/AlimtrackApplication.java
@@ -0,0 +1,13 @@
+package com.unlu.alimtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimtrackApplication.class, args);
+	}
+
+}

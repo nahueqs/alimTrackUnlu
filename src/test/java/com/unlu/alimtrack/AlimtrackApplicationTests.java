e7065e3685e204304f45376d1ac99170e02388fa
unknown <qsnahuel@gmail.com>
2025-07-17 01:58:33 -0300

    creacion repositorio

---
+++ src/test/java/com/unlu/alimtrack/AlimtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlu.alimtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

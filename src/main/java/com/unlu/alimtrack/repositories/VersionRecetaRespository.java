7990bd8c19823b5802df7e62d59558cc22b93836
Nahue <qsnahuel@gmail.com>
2025-07-23 16:50:58 -0300

    Se agrega el endpoint getVersionesByIdReceta

--- src/main/java/com/unlu/alimtrack/repositories/VersionRecetaRespository.java
+++ src/main/java/com/unlu/alimtrack/repositories/VersionRecetaRespository.java
@@ -6,10 +6,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VersionRecetaRespository extends JpaRepository<VersionRecetaModel, Long> {
 
     @Query(value = "SELECT v FROM VersionRecetaModel v WHERE v.id = :idVersion AND v.creadoPor.id = :idRecetaPadre" )
     VersionRecetaModel findByIdRecetaPadreAndIdVersion(@Param("idRecetaPadre") Long idRecetaPadre,@Param("idVersion") Long idVersion);
 
+
+    @Query(value = "SELECT v FROM VersionRecetaModel v WHERE v.creadoPor.id = :idReceta")
+    List<VersionRecetaModel> getVersionesByIdReceta(Long idReceta);
 }
+
+

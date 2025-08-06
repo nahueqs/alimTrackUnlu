1db35c1ef4073a2c3fdcab54211b7bc88e04651c
Nahue <qsnahuel@gmail.com>
2025-08-06 17:38:25 -0300

    Added password encryption when saving new uses

--- src/main/java/com/unlu/alimtrack/security/SecurityConfig.java
+++ src/main/java/com/unlu/alimtrack/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.unlu.alimtrack.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,15 +11,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .csrf(csrf -> csrf.disable())
-                .build();
+        return http.authorizeHttpRequests(
+                auth -> auth.anyRequest()
+                        .permitAll()).csrf(AbstractHttpConfigurer::disable).build();
     }
 
     @Bean

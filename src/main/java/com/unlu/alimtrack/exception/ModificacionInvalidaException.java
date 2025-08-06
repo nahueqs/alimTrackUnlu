c4d52a8fc8c34e0ce8217c61bdf24cc31198357a
Nahue <qsnahuel@gmail.com>
2025-08-06 18:16:18 -0300

    Refactors user management endpoints
    Updates user management endpoints to use DTOs for request and response.
    
    Introduces `UsuarioCreateDTO` and `UsuarioModifyDTO` for request validation.
    Changes the `getAllUsuarios` and `getUsuarioDtoById` endpoints to return `UsuarioResponseDTO`.
    Replaces `UsuarioModel` with `UsuarioModifyDTO` in `modificarUsuario` endpoint.
    Adds `ModificacionInvalidaException` for handling invalid modifications.
    Adds validation for user input.

---
+++ src/main/java/com/unlu/alimtrack/exception/ModificacionInvalidaException.java
@@ -0,0 +1,7 @@
+package com.unlu.alimtrack.exception;
+
+public class ModificacionInvalidaException extends RuntimeException  {
+    public ModificacionInvalidaException(String message) {
+        super(message);
+    }
+}

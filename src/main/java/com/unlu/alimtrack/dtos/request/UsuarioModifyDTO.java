9fec8dff3ee8592f456b4e164ab69960a9c951e8
Nahue <qsnahuel@gmail.com>
2025-08-07 01:39:41 -0300

    Refactors user modification endpoint.
    Updates the user modification endpoint to receive the user ID as a path variable and applies validation to the request body.
    This change improves the security and robustness of the user modification process by ensuring that only valid data is used to update user information.
    It also throws exception when a user tries to modify other user data by id.
    Removes unused mapper method.
    Borrows recetaService and versionRecetaService in usuarioService to implement delete user cascade.

--- src/main/java/com/unlu/alimtrack/dtos/request/UsuarioModifyDTO.java
+++ src/main/java/com/unlu/alimtrack/dtos/request/UsuarioModifyDTO.java
@@ -1,11 +1,10 @@
 package com.unlu.alimtrack.dtos.request;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record UsuarioModifyDTO (@Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
-                                String nombre,
-                                @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
-                                String contraseña) {
+public record UsuarioModifyDTO(@Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+                               String nombre,
+                               @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+                               String contraseña) {
 }
 

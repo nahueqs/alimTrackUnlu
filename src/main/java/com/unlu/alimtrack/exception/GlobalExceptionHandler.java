796b74b7e606b9719e43d2c53d0aecf8db6c1385
Nahue <qsnahuel@gmail.com>
2025-08-29 18:06:01 -0300

    Se agrega el path en ErrorResponse que para que cada exeception muestre tambien el path del endpoint

--- src/main/java/com/unlu/alimtrack/exception/GlobalExceptionHandler.java
+++ src/main/java/com/unlu/alimtrack/exception/GlobalExceptionHandler.java
@@ -1,50 +1,106 @@
 package com.unlu.alimtrack.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
+import static java.time.LocalTime.now;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(Exception ex, HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Error inesperado: " + ex.getClass().getSimpleName(),
+                request.getRequestURL().toString()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+
     @ExceptionHandler(RecursoNoEncontradoException.class)
-    public ResponseEntity<ErrorResponse> handleNotFound(RecursoNoEncontradoException ex) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleNotFound(RecursoNoEncontradoException ex, HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                request.getRequestURL().toString()
+        );
+
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
     @ExceptionHandler(FechaFormatException.class)
-    public ResponseEntity<ErrorResponse> handleFechaNoValida(FechaFormatException ex) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_ACCEPTABLE.value(), ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleFechaNoValida(FechaFormatException ex, HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_ACCEPTABLE.value(),
+                ex.getMessage(),
+                request.getRequestURL().toString()
+        );
+
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(error);
     }
 
-    @ExceptionHandler(RecursoYaExisteException.class)
-    public ResponseEntity<ErrorResponse> handleRecursoYaExistente(RecursoYaExisteException ex) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage());
+    @ExceptionHandler(RecursoDuplicadoException.class)
+    public ResponseEntity<ErrorResponse> handleRecursoYaExistente(RecursoDuplicadoException ex, HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.CONFLICT.value(),
+                ex.getMessage(),
+                request.getRequestURL().toString()
+        );
+
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
     }
 
     @ExceptionHandler(DatabaseException.class)
-    public ResponseEntity<String> handleDatabase(DatabaseException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en base de datos");
+    public ResponseEntity<ErrorResponse> handleDatabase(DatabaseException ex, HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                request.getRequestURL().toString()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
 
     @ExceptionHandler(InternalServiceException.class)
-    public ResponseEntity<String> handleInternal(InternalServiceException ex) {
+    public ResponseEntity<String> handleInternal(InternalServiceException ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                request.getRequestURL().toString()
+        );
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor");
     }
 
     @ExceptionHandler(OperacionNoPermitida.class)
-    public ResponseEntity<ErrorResponse> handleRecursoYaExistente(OperacionNoPermitida ex) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    public ResponseEntity<ErrorResponse> handleRecursoYaExistente(OperacionNoPermitida ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.FORBIDDEN.value(),
+                ex.getMessage(),
+                request.getRequestURL().toString()
+        );
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
     }
 
     @ExceptionHandler(ModificacionInvalidaException.class)
-    public ResponseEntity<ErrorResponse> handleRecursoYaExistente(ModificacionInvalidaException ex) {
-        ErrorResponse error = new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    public ResponseEntity<ErrorResponse> handleRecursoYaExistente(ModificacionInvalidaException ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(),
+                request.getRequestURL().toString()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }

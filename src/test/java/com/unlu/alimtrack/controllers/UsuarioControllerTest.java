a9d28551451b80cbac6396dcf42ffb5b60c0703f
nahue <qsnahuel@gmail.com>
2025-09-30 10:16:40 -0300

    Se agregan test para RecetaController y las firmas de los test en UsuarioControllerTest

--- src/test/java/com/unlu/alimtrack/controllers/UsuarioControllerTest.java
+++ src/test/java/com/unlu/alimtrack/controllers/UsuarioControllerTest.java
@@ -1,5 +1,10 @@
 package com.unlu.alimtrack.controllers;
 
+import static org.mockito.Mockito.verify;
+
+import com.unlu.alimtrack.dtos.response.UsuarioResponseDTO;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,4 +19,56 @@ public class UsuarioControllerTest {
 
     @InjectMocks
     private UsuarioController usuarioController;
+
+
+    @Test
+    public void testGetAllUsuarios() {
+        List<UsuarioResponseDTO> usuarios = List.of(
+            new UsuarioResponseDTO(
+                ""
+
+            );
+        );
+
+        usuarioController.getAllUsuarios();
+        verify(usuarioService).getAllUsuarios();
+
+
+
+
+    }
+
+    @Test
+    public void testSaveUsuario()
+    {
+
+    }
+
+    @Test
+    public void testUpdateUsuario() {
+
+    }
+
+    @Test
+    public void testDeleteUsuario() {
+
+    }
+
+    @Test
+    public void testGetUsuarioByEmail() {
+
+    }
+
+//    @Test
+//    public void testGetUsuariosByTipoRolUsuario() {
+//        usuarioController.getUsuariosByTipoRolUsuario(TipoRolUsuario.ADMIN);
+//        verify(usuarioService).getUsuariosByTipoRolUsuario(TipoRolUsuario.ADMIN);
+//    }
+
+
+    @Test
+    public void testGetUsuarioByEmailAndPassword() {
+
+    }
+
 }

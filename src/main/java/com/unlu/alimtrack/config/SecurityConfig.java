2c50e8b9e85fbf57eab3f1f6409ee6f313ed180f
nahue <qsnahuel@gmail.com>
2026-01-25 01:26:40 -0300

    correccion de cors

--- src/main/java/com/unlu/alimtrack/config/SecurityConfig.java
+++ src/main/java/com/unlu/alimtrack/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://alimtrack-front-vercel.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
